76458fff71ed1d899d9a736c070c425c8305ff83
pascal.desprez <pascal.desprez@fr.ibm.com>
2022-02-06 23:09:20 +0100

    inprove tests on SqlExecutingTasklet

--- src/test/java/fr/training/springbatch/tools/tasklet/SqlExecutingTaskletTest.java
+++ src/test/java/fr/training/springbatch/tools/tasklet/SqlExecutingTaskletTest.java
@@ -14,6 +14,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.scope.context.StepContext;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.batch.test.MetaDataInstanceFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -28,7 +29,7 @@ class SqlExecutingTaskletTest {
 	private ArgumentCaptor<String> captor;
 
 	@Test
-	void execute_should_success() throws Exception {
+	void execute_should_success_nominal_scenario() throws Exception {
 		// Given
 		final StepExecution stepExecution = MetaDataInstanceFactory.createStepExecution();
 		final StepContribution contribution = new StepContribution(stepExecution);
@@ -53,6 +54,30 @@ class SqlExecutingTaskletTest {
 		verify(jdbcTemplate, times(2)).execute(captor.capture());
 		assertThat(captor.getAllValues().get(0)).isEqualTo("CREATE TABLE Foo;");
 		assertThat(captor.getAllValues().get(1)).isEqualTo("DROP TABLE Foo;");
+	}
+
+	@Test
+	void execute_should_restart_on_last_position() throws Exception {
+		// Given
+		final StepExecution stepExecution = MetaDataInstanceFactory.createStepExecution();
+		final ExecutionContext executionContext = new ExecutionContext();
+		executionContext.putInt("SqlExecutingTasklet.sql.execution.count", 1);
+		stepExecution.setExecutionContext(executionContext);
+		final StepContribution contribution = new StepContribution(stepExecution);
+		final ChunkContext context = new ChunkContext(new StepContext(stepExecution));
+
+		final SqlExecutingTasklet tasklet = new SqlExecutingTasklet();
+		tasklet.setJdbcTemplate(jdbcTemplate);
+		tasklet.setSqlCommands("CREATE TABLE Foo;", "DROP TABLE Foo;");
+
+		// When
+		final RepeatStatus status = tasklet.execute(contribution, context);
+
+		// Then
+		assertThat(status).isEqualTo(RepeatStatus.FINISHED);
+
+		verify(jdbcTemplate, times(1)).execute(captor.capture());
+		assertThat(captor.getAllValues().get(0)).isEqualTo("DROP TABLE Foo;");
 	}
 
 }
